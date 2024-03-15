cc53b139a2bbbe7141019456ea8d57096101ac31
Ti Guo <guoty3310@gmail.com>
2024-03-14 23:00:32 -0400

    update codebase with algorithm for simple search on dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,19 +20,21 @@ from langchain_core.runnables import RunnablePassthrough
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
-def generate_response(file_name, openai_api_key, query_text):
+import pandas as pd
+
+def generate_response(company_info, openai_api_key, query_text):
     # Load document if file is uploaded
-    # if file_name is not None:
+    # if company_info is not None:
     # Path to the local document file
     
-    current_directory = os.getcwd() 
+    # current_directory = os.getcwd() 
 
-    # Path to the local document file in the same folder
-    # local_document_path = os.path.join(current_directory , 'docs/' + file_name)
+    # Path to the local document file in the same folder, if file is locally stored
+    # local_document_path = os.path.join(current_directory , 'docs/' + company_info)
 
     # Read the contents of the local document
     text = ''
-    for file in file_name:
+    for file in company_info:
         with pdfplumber.open(file) as pdf:
             for page in pdf.pages:
                 text += page.extract_text()
@@ -43,7 +45,7 @@ def generate_response(file_name, openai_api_key, query_text):
     # Split documents into chunks
     # Initialize the CharacterTextSplitter with best practices
     # Optimal chunk size and overlap for context preservation
-    chunk_size = 1000  # Adjust based on your document and model's token limit #TODO: change this at the end
+    chunk_size = 300  # Adjust based on your document and model's token limit 
     chunk_overlap = 50  # Adjust based on average sentence length in your text
     separator = "."  # Adjust based on your document's structure
 
@@ -98,24 +100,29 @@ def safe_list_get(l, idx, default):
   except IndexError:
     return default
     
-def get_references_text(file_name, response):
-  references = '\n\n'.join(f"[{idx+1}] ({file_name}) {safe_list_get(response.get('context', []), idx, {}).page_content}" for idx in range(len(response.get('context', []))))
-  return references
+def get_references_text(response):
+    reference_list = [safe_list_get(response.get('context', []), idx, {}).page_content for idx in range(len(response.get('context', [])))]
+
+    reference_list = list(dict.fromkeys(reference_list))
+    
+    references_text = '\n\n'.join(f"[{idx+1}] {reference_list[idx]}" for idx in range(len(reference_list)))
+
+    return references_text
 
 # Load environment variables from .env file
 load_dotenv()
 openai_api_key = os.getenv('OPENAI_API_KEY')
 
 # Page title
-st.set_page_config(page_title='Ontario Home Compliance AI Search')
-st.title('Ontario Home Compliance AI Search')
+st.set_page_config(page_title='Form Autofill')
+st.title('Form Autofill')
 
 # Add multi-select widget for selecting document
 # documentation: st.multiselect(label, options, default=None, format_func=special_internal_function, key=None, help=None, on_change=None, args=None, kwargs=None, *, max_selections=None, placeholder="Choose an option", disabled=False, label_visibility="visible")
-docs_selected = st.multiselect(
-    'Pick the documents that you like to ask your questions to:',
-    ['Ontario New Home Warranties Plan Act.txt', 'New Home Construction Licensing Act.txt', 'Application for Licences - Ontario Regulation 631.txt'],
-    ['New Home Construction Licensing Act.txt'])
+# docs_selected = st.multiselect(
+#     'Pick the documents that you like to ask your questions to:',
+#     ['Ontario New Home Warranties Plan Act.txt', 'New Home Construction Licensing Act.txt', 'Application for Licences - Ontario Regulation 631.txt'],
+#     ['New Home Construction Licensing Act.txt'])
 # st.write('You selected:', docs_selected)
 #NOTE: currently the options are not used, below suduocode is the plan for doing this: 
 # docs_selected = ['doc1.txt', 'doc2.txt']
@@ -126,25 +133,69 @@ docs_selected = st.multiselect(
 
 # final_answer = openai.generate_response(GPT_combined_answer_prompt)
 
-# File upload
-file_name = st.file_uploader('Our upload your own document', type='pdf', accept_multiple_files = True)
-# file_name = "New Home Construction Licensing Act.txt" #TODO: remove this at the end
+######## UI for User Input Data ########
+# Upload Instruction Document
+instruction_document = st.file_uploader('Upload Instruction Document', type='pdf', accept_multiple_files=True, key='instruction_document_uploader')
+# Upload Form to fill
+form_document = st.file_uploader('Upload Form Document', type='csv', key='form_document_uploader')
 
-# Query text
-#NOTE: add this (disabled=not file_name) into the st.text_input() to stop flow from continuing
-query_text = st.text_input('Enter your question:', placeholder='Please provide a short summary.') + "Please point out the section number and the specific actionable items that are relevant to the question. specifically information to do with the days"
+# Add a file input for the company info
+company_info = st.file_uploader('Upload Company Information Documents', type='pdf', accept_multiple_files=True, key='company_info_document_uploader')
+
+######## UI for table display ########
+
+# Create an empty placeholder for table
+placeholder = st.empty()
+
+# Assuming form_document is the file uploaded via st.file_uploader
+if company_info is not None:
+    if form_document is not None:
+    # Read the uploaded CSV file into a DataFrame
+        df = pd.read_csv(form_document)
+        df['Answers'] = ''  
+        df['References'] = ''
+        # Update the placeholder with the new DataFrame
+        placeholder.dataframe(df)
+    
+
+######## Logic for searching dataframe ########
+
+# Create a button in the sidebar
+data_search_button = st.button('Click to Auto-fill Form with Company Info')
+
+if data_search_button and 'df' in locals() and form_document is not None and company_info is not None:
+    for index, row in df.iterrows():
+        # Use the generate_response function to search for text in company_info within each row of the DataFrame
+        response = generate_response(company_info, openai_api_key, row['Questions'])
+        # Update the 'Answers' column
+        df.at[index, 'Answers'] = response.get('answer',"")
+        df.at[index, 'References'] = get_references_text(response)
+        # Update the placeholder with the new DataFrame
+        placeholder.dataframe(df)
+
+
+######## Logic for download final filled form ########
+# Add a download button for the DataFrame
+if 'df' in locals():
+    # Convert DataFrame to CSV string
+    csv = df.to_csv(index=False).encode('utf-8')
+    st.download_button(
+        label="Download data as CSV",
+        data=csv,
+        file_name='edited_data.csv',
+        mime='text/csv',
+    )
 
 
 # Form input and query
-result = []
-with st.form('myform', clear_on_submit=True):
-    submitted = st.form_submit_button('Submit', disabled=not(query_text))
-    if submitted and openai_api_key.startswith('sk-'):
-        with st.spinner('Calculating...'):
-            response = generate_response(file_name, openai_api_key, query_text)
-            result.append(response)
+# result = []
+# with st.form('myform', clear_on_submit=True):
+#     submitted = st.form_submit_button('Submit', disabled=not(query_text))
+#     if submitted and openai_api_key.startswith('sk-'):
+#         with st.spinner('Calculating...'):
+#             response = generate_response(company_info, openai_api_key, query_text)
+#             result.append(response)
 
-if len(result):
-    st.info(response.get('answer',""))
-    st.info('\n\nReferences:\n' + get_references_text(file_name, response))
-
+# if len(result):
+#     st.info(response.get('answer',""))
+#     st.info('\n\nReferences:\n' + get_references_text(instruction_document, response))
